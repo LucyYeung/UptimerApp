44a0cbda2ec1335156b2f9ea324268ae91137bff
Lucy <lucy70381@kimo.com>
2024-08-19 01:25:18 +0800

    feat: add notificationSchema

--- uptimer-server/src/graphql/schema/index.ts
+++ uptimer-server/src/graphql/schema/index.ts
@@ -1,5 +1,6 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
+import { notificationSchema } from './notification';
 import { userSchema } from './user';
 
-export const mergedGQLSchema = mergeTypeDefs([userSchema]);
+export const mergedGQLSchema = mergeTypeDefs([userSchema, notificationSchema]);
