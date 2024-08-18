7a8c78042d3ad0b5c6f568c4ea8fedae3e1b4208
Lucy <lucy70381@kimo.com>
2024-08-19 06:06:35 +0800

    feat: add monitor schema

--- uptimer-server/src/graphql/schema/index.ts
+++ uptimer-server/src/graphql/schema/index.ts
@@ -1,6 +1,11 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
+import { monitorSchema } from './monitor';
 import { notificationSchema } from './notification';
 import { userSchema } from './user';
 
-export const mergedGQLSchema = mergeTypeDefs([userSchema, notificationSchema]);
+export const mergedGQLSchema = mergeTypeDefs([
+  userSchema,
+  notificationSchema,
+  monitorSchema,
+]);
