72114d68a349fb47372a07b344c644cf15ef8ed9
Lucy <lucy70381@kimo.com>
2024-08-18 08:24:25 +0800

    feat: add databaseConnection in initializeApp

--- uptimer-server/src/index.ts
+++ uptimer-server/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import MonitorServer from './server/server';
+import { databaseConnection } from './server/database';
 
 const initializeApp = () => {
   const app = express();
   const monitorServer = new MonitorServer(app);
+  databaseConnection();
   monitorServer.start();
 };
 
