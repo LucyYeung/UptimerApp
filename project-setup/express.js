83dc493e6ce486d86e80f9c4eefba66add606915
Lucy <lucy70381@kimo.com>
2024-08-21 04:39:23 +0800

    feat: add express environment for testing tcp

---
+++ project-setup/express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.use((req, res, next) => {
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  next();
+})
+
+app.get('/health', (req, res) => {
+  res.send(`Server running on port: ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+})
