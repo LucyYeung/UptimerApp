f3df3f0fa8d8419c8d36e377a0256161b89d3f16
Lucy <lucy70381@kimo.com>
2024-08-21 15:50:41 +0800

    feat: add SSL monitor resolver

--- uptimer-server/src/graphql/resolvers/index.ts
+++ uptimer-server/src/graphql/resolvers/index.ts
@@ -1,6 +1,7 @@
 import { HeartbeatResolver } from './heartbeat';
 import { MonitorResolver } from './monitor';
 import { NotificationResovler } from './notification';
+import { SSLMonitorResolver } from './ssl';
 import { UserResolver } from './user';
 
 export const resolvers = [
@@ -8,4 +9,5 @@ export const resolvers = [
   NotificationResovler,
   MonitorResolver,
   HeartbeatResolver,
+  SSLMonitorResolver,
 ];
