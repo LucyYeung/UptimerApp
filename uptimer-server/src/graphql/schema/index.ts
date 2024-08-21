e0cf93dc43a3cb182913684817470bf8bc07932b
Lucy <lucy70381@kimo.com>
2024-08-21 15:50:37 +0800

    feat: add SSL monitor schema

--- uptimer-server/src/graphql/schema/index.ts
+++ uptimer-server/src/graphql/schema/index.ts
@@ -3,6 +3,7 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { heartbeatSchema } from './heartbeat';
 import { monitorSchema } from './monitor';
 import { notificationSchema } from './notification';
+import { sslMonitorSchema } from './ssl';
 import { userSchema } from './user';
 
 export const mergedGQLSchema = mergeTypeDefs([
@@ -10,4 +11,5 @@ export const mergedGQLSchema = mergeTypeDefs([
   notificationSchema,
   monitorSchema,
   heartbeatSchema,
+  sslMonitorSchema,
 ]);
