12ae0b7c84716fb26dc746fac544a2ffa43dd7ff
Lucy <lucy70381@kimo.com>
2024-08-25 20:00:38 +0800

    feat: add TextAreaInput component

---
+++ uptimer-client/src/components/TextAreaInput.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactElement } from 'react';
+
+import { ITextInputProps } from '@/interfaces/input.interface';
+
+const TextAreaInput: FC<ITextInputProps<HTMLTextAreaElement>> = (
+  props
+): ReactElement => {
+  return (
+    <textarea
+      id={props.id}
+      name={props.name}
+      value={props.value}
+      readOnly={props.readOnly}
+      className={props.className}
+      maxLength={props.maxLength}
+      style={props.style}
+      rows={props.rows}
+      placeholder={props.placeholder}
+      onChange={props.onChange}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      onKeyUp={props.onKeyUp}
+      autoComplete='false'
+    />
+  );
+};
+
+export default TextAreaInput;
