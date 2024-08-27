3e637e9c8a8e7b00aa28c985ab5e46535e70dc9d
Lucy <lucy70381@kimo.com>
2024-08-27 20:40:33 +0800

    feat: add InitialUpdateType, IUser interface

--- uptimer-client/src/interfaces/user.interface.ts
+++ uptimer-client/src/interfaces/user.interface.ts
@@ -2,6 +2,22 @@ import { Dispatch, SetStateAction } from 'react';
 
 import { RegisterType } from '@/app/(auth)/validations/auth';
 
+import { INotification } from './notification.interface';
+
+export interface InitialUpdateType {
+  notifications: INotification[];
+  user: IUser | null;
+}
+
+export interface IUser {
+  __typename?: string;
+  id?: number;
+  username?: string;
+  email?: string;
+  password?: string;
+  createdAt?: Date;
+}
+
 export interface IUserAuth {
   loading: boolean;
   validationErrors?: RegisterType;
