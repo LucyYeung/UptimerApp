e5ac3d0310d920d35cc41cf62e0648d154947e3b
Lucy <lucy70381@kimo.com>
2024-08-29 07:38:25 +0800

    feat: add count method in HomeButtonGroup

--- uptimer-client/src/app/status/components/HomeButtonGroup.tsx
+++ uptimer-client/src/app/status/components/HomeButtonGroup.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactElement } from 'react';
 
 import { IMonitorDocument } from '@/interfaces/monitor.interface';
+import { filter } from 'lodash';
 import { FaArrowDown, FaArrowUp, FaPlay } from 'react-icons/fa';
 
 import Button from '@/components/Button';
@@ -10,25 +11,38 @@ interface IHomeButtonGroupProps {
 }
 
 const HomeButtonGroup: FC<IHomeButtonGroupProps> = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   monitors,
 }): ReactElement => {
+  const count = (type: string) => {
+    let sum = 0;
+    if (type === 'active') {
+      sum = filter(monitors, { active: true, status: 0 }).length;
+    }
+    if (type === 'inactive') {
+      sum = filter(monitors, { active: false }).length;
+    }
+    if (type === 'error') {
+      sum = filter(monitors, { active: true, status: 1 }).length;
+    }
+    return sum;
+  };
+
   return (
     <div className='inline-flex' role='group'>
       <Button
-        label={1}
+        label={count('active')}
         icon={<FaArrowUp className='mr-1' />}
         type='button'
         className='mr-1 inline-flex items-center rounded bg-green-400 px-4 py-2 text-sm font-bold text-white'
       />
       <Button
-        label={1}
+        label={count('inactive')}
         icon={<FaArrowDown className='mr-1' />}
         type='button'
         className='mr-1 inline-flex items-center rounded bg-red-400 px-4 py-2 text-sm font-bold text-white'
       />
       <Button
-        label={1}
+        label={count('error')}
         icon={<FaPlay className='mr-1' />}
         type='button'
         className='mr-1 inline-flex items-center rounded bg-yellow-400 px-4 py-2 text-sm font-bold text-white'
