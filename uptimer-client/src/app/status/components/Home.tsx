70f6b82befc17d04e28619dfac2c6107855a728d
Lucy <lucy70381@kimo.com>
2024-08-29 15:03:45 +0800

    feat: add useHome in Home

--- uptimer-client/src/app/status/components/Home.tsx
+++ uptimer-client/src/app/status/components/Home.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { FC, ReactElement, useState } from 'react';
+import { FC, ReactElement } from 'react';
 
-import { IMonitorState } from '@/interfaces/monitor.interface';
-
+import { useHome } from '../hooks/useHome';
 import {
   renderButtons,
   renderRefreshButtons,
@@ -11,21 +10,53 @@ import {
 } from './HomeComponents';
 
 const Home: FC = (): ReactElement => {
-  const [monitorState, setMonitorState] = useState<IMonitorState>({
-    showModal: false,
-    enableRefresh: false,
-    autoRefreshLoading: false,
-  });
+  const {
+    monitorState,
+    monitors,
+    limit,
+    isRefreshed,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    autoMonitorsRef,
+    monitorsRef,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    openModal,
+    view,
+    loading,
+    setView,
+    setMonitors,
+    updateLimit,
+    setMonitorState,
+    refreshMonitors,
+    enableAutoRefresh,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    closeUptimeModal,
+  } = useHome();
 
   return (
     <div className='relative m-auto h-screen min-h-screen px-6 xl:container md:px-12 lg:px-6'>
-      {renderButtons([], monitorState, setMonitorState)}
-      {renderRefreshButtons('box', true)}
-      {renderTableAndPagination(
-        'box',
-        { start: 0, end: 10 },
-        monitorState.autoRefreshLoading,
-        []
+      {!loading && monitors.length > 0 ? (
+        <>
+          {renderButtons(monitors, monitorState, setMonitorState)}
+          {renderRefreshButtons(
+            view,
+            isRefreshed!,
+            monitorsRef.current,
+            monitors,
+            setView,
+            setMonitors,
+            () => refreshMonitors(),
+            () => enableAutoRefresh()
+          )}
+          {renderTableAndPagination(
+            view,
+            limit,
+            monitorState.autoRefreshLoading,
+            monitors,
+            updateLimit
+          )}
+        </>
+      ) : (
+        <>{!loading && !monitors.length && <>New Uptime Test</>}</>
       )}
     </div>
   );
