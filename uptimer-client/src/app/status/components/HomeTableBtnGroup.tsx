65e29f1800e670bc70874d700d592df1bb0d29d5
Lucy <lucy70381@kimo.com>
2024-08-30 19:05:33 +0800

    feat: add toggle, edit, delete monitor functions

--- uptimer-client/src/app/status/components/HomeTableBtnGroup.tsx
+++ uptimer-client/src/app/status/components/HomeTableBtnGroup.tsx
@@ -5,6 +5,8 @@ import { FaPause, FaPencilAlt, FaPlay, FaTrashAlt } from 'react-icons/fa';
 
 import Button from '@/components/Button';
 
+import { useHomeTableBtnGroup } from '../hooks/useHomeTableBtnGroup';
+
 interface IHomeTableBtnGroupProps {
   monitor: IMonitorDocument;
 }
@@ -12,22 +14,28 @@ interface IHomeTableBtnGroupProps {
 const HomeTableBtnGroup: FC<IHomeTableBtnGroupProps> = ({
   monitor,
 }): ReactElement => {
+  const { toggleUserMonitor, editMonitor, deleteUserMonitor } =
+    useHomeTableBtnGroup({ monitor });
+
   return (
     <div className='inline-flex shadow-sm' role='group'>
       <Button
         icon={monitor.active ? <FaPause /> : <FaPlay />}
         type='button'
         className='mr-1 inline-flex items-center rounded border border-[#1e8dee] px-4 py-2 text-sm font-bold text-[#1e8dee] hover:bg-[#1e8dee] hover:text-white'
+        onClick={toggleUserMonitor}
       />
       <Button
         icon={<FaPencilAlt />}
         type='button'
         className='mr-1 inline-flex items-center rounded border border-[#1e8dee] px-4 py-2 text-sm font-bold text-[#1e8dee] hover:bg-[#1e8dee] hover:text-white'
+        onClick={editMonitor}
       />
       <Button
         icon={<FaTrashAlt />}
         type='button'
         className='mr-1 inline-flex items-center rounded bg-red-600 px-4 py-2 text-sm font-bold text-white hover:bg-red-400 hover:text-white'
+        onClick={deleteUserMonitor}
       />
     </div>
   );
